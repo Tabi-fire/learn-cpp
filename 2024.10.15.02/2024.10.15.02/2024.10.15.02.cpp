a95c2ccae0e87b3055b8d2577e404d881b4fa097
jjulia01 <forreplitwork@gmail.com>
2024-10-15 18:48:04 +0300

    Create 2024.10.15.02.cpp
    Added Getters and Setters for mDigit and mName

---
+++ 2024.10.15.02/2024.10.15.02/2024.10.15.02.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Example {
+    int mDigit = 0;
+    string mName;
+public:
+    void SetDigit(int Digit) { mDigit = Digit; }
+    int GetDigit() { return mDigit; }
+    void SetName(string Name) { mName = Name; }
+    string GetName() { return mName; }
+    Example(int Digit, string Name) : mDigit(Digit), mName(Name) {};
+    ~Example() {};
+};
+
+int main() {}
