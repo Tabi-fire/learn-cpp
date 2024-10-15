81b513e9a8e354c827c15284e5d5557336b29247
jjulia01 <forreplitwork@gmail.com>
2024-10-15 19:18:10 +0300

    Create 2024.10.15.04.cpp
    added many Print func

---
+++ 2024.10.15.04/2024.10.15.04/2024.10.15.04.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class A
+{
+    int value;
+public:
+	A();
+	~A();
+    void Print(double value) {
+        cout << value << endl;
+    }
+
+    void Print(string value) {
+        cout << value << endl;
+    }
+    void Print(char value) {
+        cout << value << endl;
+    }
+    void Print(int value) {
+        cout << value << endl;
+    }
+private:
+
+};
+
+A::A()
+{
+}
+
+A::~A()
+{
+}
+
+
+int main()
+{
+    A a = A();
+    a.Print(10.3);
+    a.Print(23);
+    a.Print(55);
+    a.Print(12);
+}
