85eec293f6b82cbfd4f422af8d4563ce5844a4a6
jjulia01 <forreplitwork@gmail.com>
2024-10-31 18:55:38 +0300

    Create 2024.10.31.03.cpp

---
+++ 2024.10.31.03/2024.10.31.03/2024.10.31.03.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class X
+{
+	int num;
+public:
+	int GetNum() { return num; }
+	X(int value = 5) { num = value; };
+	~X();
+
+private:
+
+};
+
+X::~X()
+{
+}
+
+int main()
+{
+	X x = X(6); // Если нет числа, то выводит 5. Если есть значение в объекте, то пишет то,которое занесено здесь
+	cout << x.GetNum();
+}
