bbcd340ef6a4427c49a72cf84d891a79ca2ce003
jjulia01 <forreplitwork@gmail.com>
2024-10-31 19:54:33 +0300

    Create 2024.10.31.05.cpp

---
+++ 2024.10.31.05/2024.10.31.05/2024.10.31.05.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Example
+{
+	int x, y;
+public:
+	Example(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	void Print() {
+		cout << "X: " << x << "\n";
+		cout << "Y: " << y << "\n";
+	}
+	Example();
+	~Example();
+
+private:
+
+};
+
+Example::Example()
+{
+}
+
+Example::~Example()
+{
+}
+
+int main()
+{
+	Example example = Example();
+	Example xy(50, 100);
+	xy.Print();
+}
