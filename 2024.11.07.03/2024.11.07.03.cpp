42ba741b3b3477f0f731e057f157204e0e3b371c
Tabi-fire <forreplitwork@gmail.com>
2024-11-07 18:24:30 +0300

    Create 2024.11.07.03.cpp

---
+++ 2024.11.07.03/2024.11.07.03.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class Example
+{
+public:
+	void Print(int num) {
+		cout << num;
+	}
+	void Print(float num) {
+		cout << num;
+	}
+	Example() {};
+	~Example() {};
+
+private:
+
+};
+
+
+int main()
+{
+	Example ex = Example();
+	ex.Print(//1 или 1.1);
+}
