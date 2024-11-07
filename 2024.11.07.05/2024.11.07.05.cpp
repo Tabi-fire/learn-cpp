13762d514c3adac40734259abef7be4095d10045
Tabi-fire <forreplitwork@gmail.com>
2024-11-07 19:01:36 +0300

    Create 2024.11.07.05.cpp

---
+++ 2024.11.07.05/2024.11.07.05.cpp
@@ -0,0 +1,37 @@
+﻿//Наследие происходит от модификатора доступа protected
+//Пример:
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	A() {};
+	~A() {};
+
+private:
+
+protected:
+	int num = 5;
+};
+
+class B : public A
+{
+public:
+	int GetStrint() {
+		return num;
+	}
+
+	B() {};
+	~B() {};
+
+private:
+
+};
+
+int main()
+{
+	B b = B();
+	cout << b.GetStrint();
+}
