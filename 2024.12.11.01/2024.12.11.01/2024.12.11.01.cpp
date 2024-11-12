084dc6d9435868068e1775189ac810ee3984ab9b
jjulia01 <forreplitwork@gmail.com>
2024-11-12 18:30:55 +0300

    Update 2024.12.11.01.cpp

--- 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
+++ 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 class BankAccount
 {
+protected:
 	int mAccountNum;
 	string mBankName;
 	string mAccountName;
@@ -26,49 +27,33 @@ private:
 
 };
 
-class DebitCard
+class DebitCard : public BankAccount
 {
-	int mAccountNum;
 	int mCardNum;
 	int mCardDuteDate;
 	int mCardCVV;
-	string mBankName;
-	string mAccountName;
-	int mBalance;
 public:
-	void SetAccountNum(int accountNum) { mAccountNum = accountNum; }
-	int GetAccountNum() { return mAccountNum; }
 	void SetCardNum(int cardNum) { mCardNum = cardNum; }
 	int GetCardNum() { return mCardNum; }
 	void SetCardLastDate(int cardDuteDate) { mCardDuteDate = cardDuteDate; }
 	int GetCardLastDate() { return mCardDuteDate; }
 	void SetCardCVV(int cardCVV) { mCardCVV = cardCVV; }
 	int GetCardCVV() { return mCardCVV; }
-	void SetAccountName(string accountName) { mAccountName = accountName; }
-	string GetAccountName() { return  mAccountName; }
-	void SetBalance(int balance) { mBalance = balance; }
-	int GetBalance() { return mBalance; }
 	DebitCard(int accountNum = 0, int cardNum = 0, int cardDuteDate = 0, int cardCVV = 0, string bankName = "", string accountName = "", int balance = 0) :
-		mAccountNum(accountNum), mCardDuteDate(cardDuteDate), mCardCVV(cardCVV), mCardNum(cardNum), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
+		mCardDuteDate(cardDuteDate), mCardCVV(cardCVV), mCardNum(cardNum) {};
 	~DebitCard() {};
 
 private:
 
 };
 
-class CreditCard
+class CreditCard : public BankAccount
 {
-	int mAccountNum;
 	int mCardNum;
 	int mCardDuteDate;
 	int mCardCVV;
-	string mBankName;
-	string mAccountName;
-	int mBalance;
 	int mCreditLimit;
 public:
-	void SetAccountNum(int accountNum) { mAccountNum = accountNum; }
-	int GetAccountNum() { return mAccountNum; }
 	void SetCardNum(int cardNum) { mCardNum = cardNum; }
 	int GetCardNum() { return mCardNum; }
 	void SetCardLastDate(int cardDuteDate) { mCardDuteDate = cardDuteDate; }
@@ -77,12 +62,10 @@ public:
 	int GetCardCVV() { return mCardCVV; }
 	void SetAccountName(string accountName) { mAccountName = accountName; }
 	string GetAccountName() { return  mAccountName; }
-	void SetBalance(int balance) { mBalance = balance; }
-	int GetBalance() { return mBalance; }
 	void SetCreditLimit(int creditLimit) { mCreditLimit = creditLimit; }
 	int GetCreditLimit() { return mCreditLimit; }
 	CreditCard(int accountNum = 0, int cardNum = 0, int cardDuteDate = 0, int cardCVV = 0, string bankName = "", string accountName = "", int balance = 0, int creditLimit = 0) :
-		mAccountNum(accountNum), mCardDuteDate(cardDuteDate), mCardCVV(cardCVV), mCardNum(cardNum), mBankName(bankName), mAccountName(accountName), mBalance(balance), mCreditLimit(creditLimit) {};
+		mCardDuteDate(cardDuteDate), mCardCVV(cardCVV), mCardNum(cardNum), mCreditLimit(creditLimit) {};
 	~CreditCard() {};
 
 private:
