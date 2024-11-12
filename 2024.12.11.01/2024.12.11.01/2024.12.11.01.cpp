c2609e4b6705f8fa47297fc318dca46248df3dff
jjulia01 <forreplitwork@gmail.com>
2024-11-12 18:02:22 +0300

    Update 2024.12.11.01.cpp

--- 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
+++ 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
@@ -7,22 +7,19 @@ using namespace std;
 class BankAccount
 {
 	int mAccountNum;
-	string mCardName;
 	string mBankName;
 	string mAccountName;
 	int mBalance;
 public:
 	void SetAccountNum(int accountNum) { mAccountNum = accountNum; }
 	int GetAccountNum() { return mAccountNum; }
-	void SetCardName(string cardName) { mBankName = cardName; }
-	string GetCardName() { return mBankName; }
 	void SetBankName(string bankName) { mBankName = bankName; }
 	string GetBankName() { return mBankName; }
 	void SetAccountName(string accountName) { mAccountName = accountName; }
 	string GetAccountName() { return  mAccountName; }
 	void SetBalance(int balance) { mBalance = balance; }
 	int GetBalance() { return mBalance; }
-	BankAccount(int accountNum = 0, string cardName = "", string bankName = "", string accountName = "", int balance = 0) : mAccountNum(accountNum), mCardName(cardName), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
+	BankAccount(int accountNum = 0, string cardName = "", string bankName = "", string accountName = "", int balance = 0) : mAccountNum(accountNum), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
 	~BankAccount() {};
 
 private:
@@ -32,11 +29,20 @@ private:
 class DebitCard
 {
 	int mAccountNum;
+	string mCardName;
 	string mBankName;
 	string mAccountName;
 	int mBalance;
 public:
-	DebitCard() {};
+	void SetAccountNum(int accountNum) { mAccountNum = accountNum; }
+	int GetAccountNum() { return mAccountNum; }
+	void SetCardName(string cardName) { mBankName = cardName; }
+	string GetCardName() { return mBankName; }
+	void SetAccountName(string accountName) { mAccountName = accountName; }
+	string GetAccountName() { return  mAccountName; }
+	void SetBalance(int balance) { mBalance = balance; }
+	int GetBalance() { return mBalance; }
+	DebitCard(int accountNum = 0, string cardName = "", string bankName = "", string accountName = "", int balance = 0) : mAccountNum(accountNum), mCardName(cardName), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
 	~DebitCard() {};
 
 private:
