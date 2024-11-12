8c0df02b522947732a76b698120c1f6ddf5123d9
jjulia01 <forreplitwork@gmail.com>
2024-11-12 18:00:35 +0300

    Update 2024.12.11.01.cpp

--- 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
+++ 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
@@ -1,29 +1,47 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class BankAccount
 {
 	int mAccountNum;
+	string mCardName;
 	string mBankName;
 	string mAccountName;
 	int mBalance;
 public:
 	void SetAccountNum(int accountNum) { mAccountNum = accountNum; }
 	int GetAccountNum() { return mAccountNum; }
+	void SetCardName(string cardName) { mBankName = cardName; }
+	string GetCardName() { return mBankName; }
 	void SetBankName(string bankName) { mBankName = bankName; }
 	string GetBankName() { return mBankName; }
 	void SetAccountName(string accountName) { mAccountName = accountName; }
 	string GetAccountName() { return  mAccountName; }
 	void SetBalance(int balance) { mBalance = balance; }
 	int GetBalance() { return mBalance; }
-	BankAccount(int accountNum = 0, string bankName = "", string accountName = "", int balance = 0) : mAccountNum(accountNum), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
+	BankAccount(int accountNum = 0, string cardName = "", string bankName = "", string accountName = "", int balance = 0) : mAccountNum(accountNum), mCardName(cardName), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
 	~BankAccount() {};
 
 private:
 
 };
 
+class DebitCard
+{
+	int mAccountNum;
+	string mBankName;
+	string mAccountName;
+	int mBalance;
+public:
+	DebitCard() {};
+	~DebitCard() {};
+
+private:
+
+};
 
 
 int main()
