d22cd64411a51ec72fc08ee133d09c2a41f0730f
jjulia01 <forreplitwork@gmail.com>
2024-11-12 18:14:16 +0300

    Update 2024.12.11.01.cpp

--- 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
+++ 2024.12.11.01/2024.12.11.01/2024.12.11.01.cpp
@@ -30,7 +30,7 @@ class DebitCard
 {
 	int mAccountNum;
 	int mCardNum;
-	int mCardLastDate;
+	int mCardDuteDate;
 	int mCardCVV;
 	string mBankName;
 	string mAccountName;
@@ -38,17 +38,18 @@ class DebitCard
 public:
 	void SetAccountNum(int accountNum) { mAccountNum = accountNum; }
 	int GetAccountNum() { return mAccountNum; }
-	void SetCardName(int cardNum) { mCardNum = cardNum; }
-	int GetCardName() { return mCardNum; }
-	void SetCardLastDate(int cardLastDate) { mCardLastDate = cardLastDate; }
-	int GetCardLastDate() { return mCardLastDate; }
+	void SetCardNum(int cardNum) { mCardNum = cardNum; }
+	int GetCardNum() { return mCardNum; }
+	void SetCardLastDate(int cardDuteDate) { mCardDuteDate = cardDuteDate; }
+	int GetCardLastDate() { return mCardDuteDate; }
 	void SetCardCVV(int cardCVV) { mCardCVV = cardCVV; }
 	int GetCardCVV() { return mCardCVV; }
 	void SetAccountName(string accountName) { mAccountName = accountName; }
 	string GetAccountName() { return  mAccountName; }
 	void SetBalance(int balance) { mBalance = balance; }
 	int GetBalance() { return mBalance; }
-	DebitCard(int accountNum = 0, int cardName = 0, int cardLastDate = 0, int cardCVV = 0, string bankName = "", string accountName = "", int balance = 0) : mAccountNum(accountNum), mCardName(cardName), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
+	DebitCard(int accountNum = 0, int cardNum = 0, int cardDuteDate = 0, int cardCVV = 0, string bankName = "", string accountName = "", int balance = 0) :
+		mAccountNum(accountNum), mCardDuteDate(cardDuteDate), mCardCVV(cardCVV), mCardNum(cardNum), mBankName(bankName), mAccountName(accountName), mBalance(balance) {};
 	~DebitCard() {};
 
 private:
