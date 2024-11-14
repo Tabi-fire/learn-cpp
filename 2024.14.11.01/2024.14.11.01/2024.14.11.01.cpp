0353dd6a9d868065b49341896e43a43496598c3c
jjulia01 <forreplitwork@gmail.com>
2024-11-14 17:45:02 +0300

    Create 2024.14.11.01.cpp

---
+++ 2024.14.11.01/2024.14.11.01/2024.14.11.01.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+	string word;
+public:
+	Animal(string w) : word(w) {};
+	void Say() { cout << word << "\n"; }
+};
+
+class Dog : public Animal {
+public:
+	Dog() : Animal("Гав") {};
+	void Bark() { cout << "Я гавкаю, потому что я Собака\n"; }
+};
+
+
+void Say(Animal* animal) { 
+	// перечислите методы, доступные объекту animal
+	// объект animal имеет только один метод 'Say()'
+
+}
+
+int main()
+{
+
+}
