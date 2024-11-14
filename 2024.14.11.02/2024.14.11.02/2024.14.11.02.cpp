87ae542a7a8bd15e4130a669fa97c0a4a07aa305
jjulia01 <forreplitwork@gmail.com>
2024-11-14 18:51:39 +0300

    Update 2024.14.11.02.cpp

--- 2024.14.11.02/2024.14.11.02/2024.14.11.02.cpp
+++ 2024.14.11.02/2024.14.11.02/2024.14.11.02.cpp
@@ -10,8 +10,12 @@ protected:
 public:
 	void SetHomeAnimals(string name) { mName = name; }
 	string GetHomeAnimals() { return mName; }
+	void Say(string name) {
+		cout << name << endl;
+	}
 	HomeAnimals(string name = "") : mName(name) {};
 	~HomeAnimals() {};
+
 
 private:
 
@@ -24,9 +28,7 @@ class Dog : public HomeAnimals
 public:
 	Dog(string name) : HomeAnimals(name) {};
 	~Dog() {};
-	void Say() {
-		cout << mName << endl;
-	}
+	
 private:
 
 };
@@ -36,9 +38,6 @@ class Cat : public HomeAnimals
 public:
 	Cat(string name) : HomeAnimals(name) {};
 	~Cat() {};
-	void Say() {
-		cout << mName << endl;
-	}
 private:
 
 };
@@ -48,9 +47,6 @@ class Parrot : public HomeAnimals
 public:
 	Parrot(string name) : HomeAnimals(name) {};
 	~Parrot() {};
-	void Say() {
-		cout << mName << endl;
-	}
 private:
 
 };
@@ -61,7 +57,7 @@ int main()
 	Dog dog = Dog("Dog");
 	Cat cat = Cat("Cat");
 	Parrot parrot = Parrot("Parrot");
-	dog.Say();
-	cat.Say();
-	parrot.Say();
+	dog.Say("Dog");
+	cat.Say("Cat");
+	parrot.Say("Parrot");
 }
