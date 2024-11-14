37ee4752472b3855ac61e3b41057015562ccee12
jjulia01 <forreplitwork@gmail.com>
2024-11-14 18:32:06 +0300

    Create 2024.14.11.02.cpp

---
+++ 2024.14.11.02/2024.14.11.02/2024.14.11.02.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class HomeAnimals
+{
+
+protected:
+	string mName;
+public:
+	void SetHomeAnimals(string name) { mName = name; }
+	string GetHomeAnimals() { return mName; }
+	HomeAnimals(string name = "") : mName(name) {};
+	~HomeAnimals() {};
+
+private:
+
+
+};
+
+class Dog : public HomeAnimals
+{
+
+public:
+	Dog(string name) : HomeAnimals(name) {};
+	~Dog() {};
+	void Say() {
+		cout << mName << endl;
+	}
+private:
+
+};
+
+class Cat : public HomeAnimals
+{
+public:
+	Cat(string name) : HomeAnimals(name) {};
+	~Cat() {};
+	void Say() {
+		cout << mName << endl;
+	}
+private:
+
+};
+
+class Parrot : public HomeAnimals
+{
+public:
+	Parrot(string name) : HomeAnimals(name) {};
+	~Parrot() {};
+	void Say() {
+		cout << mName << endl;
+	}
+private:
+
+};
+
+
+int main()
+{
+	Dog dog = Dog("Dog");
+	Cat cat = Cat("Cat");
+	Parrot parrot = Parrot("Parrot");
+	dog.Say();
+	cat.Say();
+	parrot.Say();
+}
